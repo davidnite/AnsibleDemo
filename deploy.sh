28605149fca8b41f3ec1817fd6f8c966cab5e03a
davidnite <davidnite@me.com>
2018-11-29 15:08:12 -0600

    fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 ansible-playbook 0-prereq.yml --extra-vars "@vars.yml"
-ansible-playbook 0-vmdeploy.yml --extra-vars "@vars.yml"
-ansible-playbook 0-appinstall.yml --extra-vars "@vars.yml"
+ansible-playbook 1-vmdeploy.yml --extra-vars "@vars.yml"
+ansible-playbook 2-appinstall.yml --extra-vars "@vars.yml"

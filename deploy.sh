c82278066f793e8ae36fb0fe1102a8ecd29b539a
davidnite <davidnite@me.com>
2018-11-29 16:21:36 -0600

    add ssh key generation to script

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
-
+ssh_key=$HOME/.ssh/id_rsa
+if [ -e "$ssh_key" ]; then
+    echo "ssh key already exists"
+else 
+    echo "ssh key does not exist"
+    echo "creating ssh key"
+    ssh-keygen -t rsa -q -P "" -f $HOME/.ssh/id_rsa
+fi
 ansible-playbook 0-prereq.yml --extra-vars "@vars.yml"
 ansible-playbook 1-vmdeploy.yml --extra-vars "@vars.yml"
 ansible-playbook 2-appinstall.yml --extra-vars "@vars.yml"
